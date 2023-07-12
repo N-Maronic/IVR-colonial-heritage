24f3865484251d19093c883050bebd92d74ffb7a
Naomi <90697728+khaleesi753@users.noreply.github.com>
2023-07-12 12:50:00 +0200

    Update answer_phone.py
    English version works

--- quickstart/answer_phone.py
+++ quickstart/answer_phone.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, url_for
 from twilio.twiml.voice_response import VoiceResponse, Gather
 
 app = Flask(__name__)
@@ -9,84 +9,63 @@ def welcome():
     response = VoiceResponse()
 
     message = """Hello, thank you for participating at identifying and describing objects from Ghana that are now displayed in Dutch Museums. 
-                 For each of the five objects we will ask you four questions. Please answer the questions to the best of your abilities. 
-                 After every question, we will record your answer for at most 2 minutes. 
-                 If you are done earlier, you can press zero to continue with the next question. 
+                 For each of the four objects we will ask you three questions. Please answer the questions to the best of your abilities. 
                  We will evaluate your recordings and forward our results to the museum, hence they can add new information about the objects to their database. """
 
 
-    # If Twilio's request to our app included already gathered digits,
-    # process them
-    if 'Digits' in request.values:
-        # Get which digit the caller chose
-        choice = request.values['Digits']
+    response.say(message)
+    response.say("Let's get started! Please record your answer after the beep and then hit the pound sign. ")
 
-        # <Say> a different message depending on the caller's choice
-        if choice == '1':
-            # response.say("you chose one")
-            survey(response)
-            return str(response)
-        else:
-            response.say("Sorry, I don't understand that choice.")
-    else:
-        response.say(message)
+    # Start with first question for first object
+    response.redirect('/question/1/1')
 
-    # Start our <Gather> verb
-    gather = Gather(num_digits=1)
-    gather.say('Press one, to start with the first object.')
-    response.append(gather)
-
-    # If the user doesn't select an option, redirect them into a loop
-    response.redirect('/welcome')
-    
     return str(response)
 
 
-# private methods
+@app.route("/question/<question_id>/<obj_id>", methods=['POST'])
+def question(question_id, obj_id):
+    questions = ["What do you see? Please describe the object. ",
+                 "According to you, what is the purpose of this object? What could it be used for? ",
+                 "Do you know about any rules in terms of how the object needs to be treated? What should, or should not be done with it? "]
 
-def survey(response):
-    questions = ["What do you see? Please describe the object.",
-                 "According to you, what is the purpose of this object? What could it be used for?",
-                 "Do you know about any rules in terms of how the object needs to be treated? What should, or should not be done with it?"]
+    response = VoiceResponse()
+    # if not English (but Dagbani), instead of text-to-speech, play wav files stored in twilio assets
+        # response.play('https://avocado-ant-4965.twil.io/assets/question%201.mp3')
 
-    response.say("Let's get started! Press zero when you finished.")
+    i = int(question_id)
+    
+    output = "Question " + question_id + ": " + questions[i-1]
+    response.say(output)
+    action_url = url_for('next', question_id=question_id, obj_id=obj_id)
+    response.record(action=action_url, finishOnKey='#')
 
-    for j in range(1,5):
-        for i in range(1,4):
-            output = "Question " + str(i) + ": " + questions[i-1]
-            response.say(output)
-            # response.record(max_length=2)
+    return str(response)
 
-        if j != 4:
-            next_object = "Thank you for answering all questions about the object " + str(j) +  ". We will now continue with the next object."
-        response.say(next_object)
+@app.route("/next/<question_id>/<obj_id>", methods=['POST'])
+def next(question_id, obj_id):
+    response = VoiceResponse()
+    i = int(question_id) + 1
+    next_q = str(i)
 
-    response.say("""Thank you for completing this survey and helping us to get a better understanding
+    if question_id == '3' and obj_id == '4':
+        response.say("""Thank you for completing this survey and helping us to get a better understanding
                     for how people perceive colonial heritage objects. The call will now automatically end.""")
+        response.hangup()
+    elif question_id == '3':
+        temp = int(obj_id) + 1
+        next_o = str(temp)
+        output = "We will now continue with the object number " + next_o + " . Please record your answer after the beep and then hit the pound sign."
+        response.say(output)
+        response.redirect(url=url_for('question', question_id=1, obj_id=next_o), method='POST')
+    else:
+        response.redirect(url=url_for('question', question_id=next_q, obj_id=obj_id), method='POST')
+    return str(response)
 
-    response.hangup()
-    return response
-
-
-
-@app.route("/test", methods=['GET', 'POST'])
-def answer_call():
-    """Respond to incoming phone calls with a brief message."""
-    # Start our TwiML response
-    resp = VoiceResponse()
-
-    # Read a message aloud to the caller
-    resp.say("Test for hp project", voice='alice')
-
-    # If the user doesn't select an option, redirect them into a loop
-    resp.redirect('/test')
-
-    return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-# in quickstart env:
+# in quickstart:
 #   dir *env*
 #   . env/bin/activate
 #   python3 answer_phone.py
